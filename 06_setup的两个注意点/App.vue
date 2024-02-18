6b739b450246c856e5f7cc135d59e53ae5dd5574
cmyshare <cmyshare@163.com>
2024-02-18 21:41:53 +0800

    06_setup的两个注意点

---
+++ 06_setup的两个注意点/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Demo @hello="showHelloMsg" msg="你好啊" school="开放Vue3">
+    <template v-slot:qwe>
+      <span>开放vue3</span>
+    </template>
+    <template v-slot:asd>
+      <span>开放Vue3</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from "./components/Demo";
+export default {
+  name: "App",
+  components: { Demo },
+  setup() {
+    function showHelloMsg(value) {
+      alert(`你好啊，你触发了hello事件，我收到的参数是:${value}！`);
+    }
+    return {
+      showHelloMsg,
+    };
+  },
+};
+</script>
+
